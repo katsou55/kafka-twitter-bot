c4d573f32c09a380d3755b6063c8a9853cb4b7ed
Carlos Rodrigues <carlosfeup55@gmail.com>
2017-02-27 11:40:14 +0000

    Update TwitterSink.scala

--- src/main/scala-2.11/marionete/spike/laura/TwitterSink.scala
+++ src/main/scala-2.11/marionete/spike/laura/TwitterSink.scala
@@ -12,10 +12,6 @@ import collection.JavaConversions._
   */
 object Util {
   val config = new twitter4j.conf.ConfigurationBuilder()
-//    .setOAuthConsumerKey("aA4Tuv7NOi7JaUOUY2o2Xn7mG")
-//    .setOAuthConsumerSecret("rA0xELBxxaSJcLDeCWCSjGyuJuLGLrJ2mZOrSLwDy5ahmREEHl")
-//    .setOAuthAccessToken("769081001901514752-pGVemOGOGA2b28SS41i4LBdKUChqhqX")
-//    .setOAuthAccessTokenSecret("9yqhIOOW1I26kmUIhVPbbVi7w8E1U4J4rXzVRY6Xf2Nod")
     .setOAuthConsumerKey("v81GD2JsaXdp6hAIDwzW5ckHk")
     .setOAuthConsumerSecret("utZUUSk76G3nF7j8BMgrJYHsRFOULBLqotJH8076jbVWKrfYR6")
     .setOAuthAccessToken("829353743011442688-RvwrwwGzmIaQNRUA5Ku8HPJCb6YIgDh")
